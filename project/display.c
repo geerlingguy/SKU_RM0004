fcaf7731c09b06c8cf42e45792220a7bc50e9df3
Jeff Geerling <geerlingguy@mac.com>
2022-09-22 22:48:38 -0500

    Properly handle shutdown.

--- project/display.c
+++ project/display.c
@@ -1,32 +1,39 @@
 /******
-Demo for ssd1306 i2c driver for  Raspberry Pi 
+Demo for ssd1306 i2c driver for  Raspberry Pi
 ******/
 #include <stdio.h>
 #include "st7735.h"
 #include "time.h"
 #include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+static bool running = false;
 
+static void onterm(int signum){
+	if( signum == SIGTERM || signum == SIGINT || signum == SIGABRT ){
+		running = false;
+		lcd_fill_screen(ST7735_BLACK);
+	}
+	exit(signum);
+}
 
-int main(void) 
+int main(void)
 {
-	uint8_t symbol = 0;
-	
-	if(lcd_begin())      //LCD Screen initialization
+	signal(SIGTERM, onterm);	// Shutdown
+	signal(SIGINT, onterm);		// Ctrl+c
+	signal(SIGABRT, onterm);	// kill
+	running = true;
+	if(lcd_begin())  // LCD Screen initialization
 	{
 		return 0;
 	}
 	sleep(1);
-	while(1)
+	while(running == true)
 	{
-		lcd_display(symbol);
-		sleep(1);
-        sleep(1);
-		symbol++;
-		if(symbol==4)
-        {
-          symbol=0;
-        }
+		lcd_display_cpuLoad();
+		sleep(5);
 	}
 	return 0;
 }
