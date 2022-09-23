655c12a4d32ffba9db35b8d3747c3f17ab5be4a1
Jeff Geerling <geerlingguy@mac.com>
2022-09-22 22:48:51 -0500

    Just display CPU load, and only sometimes for now.

--- hardware/st7735/st7735.c
+++ hardware/st7735/st7735.c
@@ -256,12 +256,12 @@ void lcd_display_cpuLoad(void)
   if (IP_SWITCH == IP_DISPLAY_OPEN)
   {
     lcd_write_string(0,0,"IP:",Font_8x16,ST7735_WHITE,ST7735_BLACK);
-    strcpy(iPSource,get_ip_address());   //Get the IP address of the device's wireless network card 
-    lcd_write_string(24,0,iPSource,Font_8x16,ST7735_WHITE,ST7735_BLACK);   //Send the IP address to the lower machine
+    strcpy(iPSource,get_ip_address());  // Get the IP address of the device
+    lcd_write_string(24,0,iPSource,Font_8x16,ST7735_WHITE,ST7735_BLACK);
   }
   else
-  {   
-    lcd_write_string(0,0,CUSTOM_DISPLAY,Font_8x16,ST7735_WHITE,ST7735_BLACK);   //Send the IP address to the lower machine
+  {
+    lcd_write_string(0,0,CUSTOM_DISPLAY,Font_8x16,ST7735_WHITE,ST7735_BLACK);
   }
   lcd_write_string(36,35,"CPU:",Font_11x18,ST7735_WHITE,ST7735_BLACK);
   lcd_write_string(80,35,cpuStr,Font_11x18,ST7735_WHITE,ST7735_BLACK);
